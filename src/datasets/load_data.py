808d582b052281a1e145d90cd4d23fc32d57d62d
Nguyen Hieu <hieunp@technica.ai>
2026-05-12 11:24:01 +0700

    refactor transform

--- src/datasets/load_data.py
+++ src/datasets/load_data.py
@@ -20,26 +20,21 @@ def load_data():
     # transforms for dataset
     data_transforms = {
         'train': transforms.Compose([
-            # Cắt ngẫu nhiên và resize về IMG_SIZE
             transforms.RandomResizedCrop(IMG_SIZE),
-            # Lật ngang ngẫu nhiên (p = 0.5)
             transforms.RandomHorizontalFlip(p=0.5),
-            # Xoay ngẫu nhiên (p = 0.5)
             transforms.RandomRotation(degrees=15),
-            # Thay đổi độ sáng ngẫu nhiên, độ tương phản, độ bão hòa (p = 0.5)
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-            # Làm sắc nét ngẫu nhiên (p = 0.5)
-            transforms.RandomAdjustSharpness(5.0), 
-            transforms.ToTensor(),
-            # Chuẩn hóa ảnh với mean và std của ImageNet
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]),
-        'val': transforms.Compose([
-            transforms.Resize(IMG_SIZE),
-            transforms.CenterCrop(IMG_SIZE),
             transforms.RandomAdjustSharpness(5.0),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),          
+            transforms.CenterCrop(IMG_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
         ])
     }
 
